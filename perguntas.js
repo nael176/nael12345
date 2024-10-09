ea9d2360692f286585ea6a454520ad178956653c
nael176 <nael.barreto@escola.pr.gov.br>
2024-10-09 12:22:21 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,61 +1,61 @@
 criaCartao
- ('matemática','1. O que é uma equação do segundo grau?','É uma conta com \(x^2\), como \(x^2 + 3x + 2 = 0\).')
+    ('matemática', '1. O que é uma equação do segundo grau?', 'É uma conta com \(x^2\), como \(x^2 + 3x + 2 = 0\).')
 
 criaCartao
-('Programação','2. O que é um loop em programação?','Um loop é uma estrutura que repete um bloco de código várias vezes até que uma condição seja atendida ou deixe de ser verdadeira.')
+    ('Programação', '2. O que é um loop em programação?', 'Um loop é uma estrutura que repete um bloco de código várias vezes até que uma condição seja atendida ou deixe de ser verdadeira.')
 
 criaCartao
-('química','3. O que é um átomo?','É a menor parte que forma tudo ao nosso redor.')
+    ('química', '3. O que é um átomo?', 'É a menor parte que forma tudo ao nosso redor.')
 
 criaCartao
-('biologia','4. O que é fotossíntese?','É o processo pelo qual as plantas usam a luz do sol para fazer comida.')
+    ('biologia', '4. O que é fotossíntese?', 'É o processo pelo qual as plantas usam a luz do sol para fazer comida.')
 
 criaCartao
-('história','5. Quem foi o primeiro presidente do Brasil?','Foi o Marechal Deodoro da Fonseca.')
+    ('história', '5. Quem foi o primeiro presidente do Brasil?', 'Foi o Marechal Deodoro da Fonseca.')
 
 criaCartao
-('português','6. O que é um verbo?','É uma palavra que indica ação, como correr, falar e comer.)
+    ('português', '6. O que é um verbo?', 'É uma palavra que indica ação, como correr, falar e comer.')
 
 criaCartao
-('geografia','7. O que são os continentes','Grandes pedaços de terra como a África e a Ásia.')
+    ('geografia', '7. O que são os continentes', 'Grandes pedaços de terra como a África e a Ásia.')
 
 criaCartao
-('matemática','8. Qual é a fórmula da área de um triângulo?','É a base vezes a altura, dividido por 2.)
+    ('matemática', '8. Qual é a fórmula da área de um triângulo?', 'É a base vezes a altura, dividido por 2.')
 
 criaCartao
-('programação','9. O que é uma variável em programação?','Uma variável é um nome que usamos para guardar um valor, como um número ou uma palavra, no código.')
+    ('programação', '9. O que é uma variável em programação?', 'Uma variável é um nome que usamos para guardar um valor, como um número ou uma palavra, no código.')
 
 criaCartao
-('química','10. O que é um elemento químico?','uma substância básica, como ouro ou oxigênio.')
+    ('química', '10. O que é um elemento químico?', 'uma substância básica, como ouro ou oxigênio.')
 
 criaCartao
- ( 'Matemática',' 11. Matemática-Qual o valor de pi?',
-'3.14')
+    ('Matemática', ' 11. Matemática-Qual o valor de pi?',
+                        '3.14')
+
+criaCartao           
+    ('História', '13.Assumiu o governo, após a saída de Vargas',
+                        'Dutra')
 
 criaCartao
-('História','13.Assumiu o governo, após a saída de Vargas',
-'Dutra')
+    ('Biologia', '14. Quem foi Mendel?', 'Biólogo e botânico austríaco')
 
 criaCartao
-('Biologia','14. Quem foi Mendel?', 'Biólogo e botânico austríaco')
+    ('O que é ligação covalente?', '15. Uma ligação covalente é a união entre dois átomos que compartilham pares de elétrons para alcançar maior estabilidade.')
 
 criaCartao
-('O que é ligação covalente?','15. Uma ligação covalente é a união entre dois átomos que compartilham pares de elétrons para alcançar maior estabilidade.')
+    ('Português', '16. O que é uma metáfora?', 'Uma metáfora é uma comparação implícita entre duas coisas.')
 
 criaCartao
-('Português','16. O que é uma metáfora?','Uma metáfora é uma comparação implícita entre duas coisas.')
+    ('programação', '17.Qual é a saída do seguinte código em Python? ', ' A saída é `[1, 3, 5]`. O fatiamento `x[::2]` seleciona todos os elementos da lista `x` com um passo de 2.')
 
 criaCartao
-('programação','17.Qual é a saída do seguinte código em Python? ',' A saída é `[1, 3, 5]`. O fatiamento `x[::2]` seleciona todos os elementos da lista `x` com um passo de 2.')
+    ('química', '18.O que é um ácido?', 'É uma substância que libera íons hidrogênio (H⁺) em solução aquosa.')
 
 criaCartao
-('química','18.O que é um ácido?','É uma substância que libera íons hidrogênio (H⁺) em solução aquosa.')
+    ('história', '19. O que foi a Inconfidência Mineira?', 'Foi uma tentativa de independência em Minas Gerais no final do século XVIII contra o domínio português.')
 
 criaCartao
-('história','19. O que foi a Inconfidência Mineira?','Foi uma tentativa de independência em Minas Gerais no final do século XVIII contra o domínio português.')
+    ('Geografia', '20. O que é um rio de planície?', 'É um rio que flui por áreas baixas, com um curso largo e menos inclinado.')
 
 criaCartao
-('Geografia','20. O que é um rio de planície?','É um rio que flui por áreas baixas, com um curso largo e menos inclinado.')
-
-criaCartao
-('Biologia','Qual é a função das mitocôndrias?','Produzir energia (ATP) através da respiração celular.')
+    ('Biologia', 'Qual é a função das mitocôndrias?', 'Produzir energia (ATP) através da respiração celular.')
